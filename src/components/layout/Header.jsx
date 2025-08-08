d02dd12de66aea39c0b480edc7abea2980e8c08c
Amine4jh <amine4jh@gmail.com>
2025-08-08 22:45:13 +0100

    Create Header Component and LandingPage

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,15 @@
+const Header = ({ logo }) => {
+  return (
+    <header>
+      <img src={logo} alt="logo" width={120} />
+      <div>
+        <a href="/">Services</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+      </div>
+      <button>Try For Free!</button>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/images/logo.png";
+import Header from "../components/layout/Header";
+
+const LandingPage = () => {
+  return (
+    <div id="container">
+      <Header logo={logo} />
+    </div>
+  );
+};
+
+export default LandingPage;
