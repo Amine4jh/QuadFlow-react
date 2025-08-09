80fcc90f79b8e62bad03fc9c49fb133a5089cab1
Amine4jh <amine4jh@gmail.com>
2025-08-09 19:54:04 +0100

    Add Nav Component

---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,15 @@
+const Nav = ({ logo }) => {
+  return (
+    <nav>
+      <img src={logo} alt="logo" width={120} />
+      <div>
+        <a href="/">To-Do List</a>
+        <a href="/">Notes</a>
+        <a href="/">Calendar</a>
+        <a href="/">Pomodoro</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
