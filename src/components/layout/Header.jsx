bd433ba495f2ab60273bf4337895791351515005
Amine4jh <amine4jh@gmail.com>
2025-08-11 07:18:56 +0100

    Install react-scroll, and some links scrollable

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-scroll";
+
 const Header = ({ logo }) => {
   return (
     <header>
       <img src={logo} alt="logo" width={120} />
       <div>
-        <a href="/">Services</a>
-        <a href="/">About</a>
+        <Link to="features" spy={true} smooth={true} offset={-50} duration={500}>Services</Link>
+        <Link to="whyUs" spy={true} smooth={true} offset={-50} duration={500}>About</Link>
         <a href="/">Contact</a>
       </div>
       <button id="primary">Try For Free!</button>
